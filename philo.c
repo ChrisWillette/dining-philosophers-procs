3f63daaf668e26aa6a99d9fee8c5db31b1da75a9
ChrisWillette <christopher.willette@gmail.com>
2016-03-18 16:21:39 -0700

    derp

--- philo.c
+++ philo.c
@@ -1,1 +1,10 @@
 //chris willette
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <semaphore.h>
+#include <fcntl.h>
+
